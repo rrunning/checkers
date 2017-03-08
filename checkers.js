a691f85c63a58388193b3687182b27273af1f163
Ryan Running <ryanrunning@securitymetrics.com>
2017-03-07 19:17:44 -0700

    adds beginning movement functionality on board state array and in DOM

--- checkers.js
+++ checkers.js
@@ -1,4 +1,15 @@
 $('document').ready (function() {
+    var chosenOne;
+    var boardState = [
+        new Array(8),
+        new Array(8),
+        new Array(8),
+        new Array(8),
+        new Array(8),
+        new Array(8),
+        new Array(8),
+        new Array(8)
+    ];
     function buildBoard () {
         for(var i = 0; i < 8; i++){
             addRow(i);
@@ -7,13 +18,86 @@ $('document').ready (function() {
             };
         };
     }
-    buildBoard();
     function addRow (i) {
         var newDiv = $("<div>", {id: "row" + i, "class": "row"});
         $('#master-div').append(newDiv);
     }
     function addColumn (i, j) {
-        var newCol = $("<div>", {id: '' + i + j, 'class': 'column ' + ((i + j) % 2 === 0 ? 'red-cell' : '')});
+        var newCol = $("<div>", {id: '' + i + j, 'class': 'column ' + (squareIsRed(i, j) ? 'red-cell' : '')});
         $('#row' + i).append(newCol);
+    }
+    function squareIsRed(row, col) {
+        return (row + col) % 2 === 0;
+    }
+    function placePieces() {
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                if (squareIsRed(i, j) && i < 3){
+                    createPiece('white',false,i,j);
+                }
+                else if (squareIsRed(i, j) && i > 4) {
+                    createPiece('black',false,i,j);
+                }
+            }
+        }
+    }
+    function createPiece(color,isKing,row,col){
+        createDOMPiece(color,false,row,col);
+        createBSPiece(color, row, col);
+    }
+    function createDOMPiece(color,isKing,row,col) {
+        var piece;
+        if (color === 'white') {
+            piece = $('<img>', {src: 'assets/white-checker.png', class: 'white-checker'});
+        }
+        else if (color === 'black'){
+            piece = $('<img>', {src: 'assets/black-checker.png', class: 'black-checker'});
+        }
+        $('#' + row + col).html(piece);
+    }
+    function createBSPiece(color, row, col){
+        boardState[row][col] = {
+            color:color,
+            kinged: false,
+            row: row,
+            col: col
+        };
+    }
+    function setup() {
+        buildBoard();
+        placePieces();
+        console.log(boardState);
+    }
+    setup();
+    $('.red-cell').click(function(){
+        var id = $(this).attr('id');
+        var row = id[0];
+        var col = id[1];
+        var clickedCell = boardState[row][col];
+        console.log(clickedCell);
+        if (chosenOne) {
+            movePiece(chosenOne, row, col);
+            chosenOne = null;
+        }
+        else if (clickedCell) {
+            chosenOne = clickedCell;
+        }
+    });
+    function movePiece(piece, trgtRow, trgtCol) {
+        movePieceDOM(piece, trgtRow, trgtCol);
+        movePieceBS(piece, trgtRow, trgtCol);
+    }
+    function movePieceBS(piece, trgtRow, trgtCol) {
+        //remove piece from old location
+        boardState[piece.row][piece.col] = null;
+        //add piece to new location
+        boardState[trgtRow][trgtCol] = piece;
+        //update piece to reflect new location
+        piece.row = trgtRow;
+        piece.col = trgtCol;
+    }
+    function movePieceDOM (piece, trgtRow, trgtCol) {
+        var img = $('#' + piece.row + piece.col).find("img")
+        $('#' + trgtRow + trgtCol).append(img);
     }
 });
